e8dfcf88d38202fb82b510e78b6e752969bc6fd7
Alexander Naumov <Axeller@mail.ru>
2022-05-21 18:41:56 +0800

    feat: done 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const { stat } = require('fs/promises')
+const { readdir } = require('fs/promises')
+const path = require('path')
+const { stdout } = require('process')
+
+readdir(path.join(__dirname, 'secret-folder'), {
+  withFileTypes: true,
+}).then(files => {
+  files
+    .filter(file => !file.isDirectory())
+    .forEach(file => {
+      const filePath = path.join(
+        __dirname,
+        `./secret-folder/${file.name}`
+      )
+      const parsedFile = path.parse(filePath)
+      stat(filePath).then(file => {
+        stdout.write(
+          `${parsedFile.name} - ${parsedFile.ext.slice(
+            1
+          )} - ${file.size}b\n`
+        )
+      })
+    })
+})
